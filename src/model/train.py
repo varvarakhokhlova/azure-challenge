5584941c4ec9aad93a9659ac78d61a2a361ae203
Varvara <164140734+varvarakhokhlova@users.noreply.github.com>
2024-06-04 16:13:03 +0300

    add split data function

--- src/model/train.py
+++ src/model/train.py
@@ -5,7 +5,9 @@ import glob
 import os
 
 import pandas as pd
+import numpy as np
 
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 
@@ -33,7 +35,15 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df):
+    # split data
+    X, y = df[[
+        'Pregnancies','PlasmaGlucose','DiastolicBloodPressure',
+        'TricepsThickness','SerumInsulin','BMI','DiabetesPedigree',
+        'Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
